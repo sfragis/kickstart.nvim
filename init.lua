6178990762e990c99601d3c9df38c22d82598cdf
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2023-09-27 23:40:16 +0200

    Transparent bg and custom settings

--- init.lua
+++ init.lua
@@ -535,5 +535,7 @@ cmp.setup {
   },
 }
 
+require('custom.set')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/set.lua
@@ -0,0 +1,14 @@
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+require('rose-pine').setup({
+    disable_background = true
+})
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

0eeb061e594bd1cb4d4ae07f5a18741ef0743b6a
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2023-09-27 23:40:16 +0200

    Transparent bg and custom settings

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -3,6 +3,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+<<<<<<< HEAD
 -- use <space>h to toggle highlight
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
@@ -31,3 +32,14 @@ vim.wo.number = true
  -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
  -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "none" })
  -- vim.api.nvim_set_hl(0, "NonText", { bg = "none", fg = "none" })
+=======
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+require('rose-pine').setup({
+    disable_background = true
+})
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+>>>>>>> 55e6ee1 (Transparent bg and custom settings)

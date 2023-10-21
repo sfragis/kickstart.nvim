dbb5861d266277c620f2c009e82c963716b46768
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2023-10-21 22:10:17 +0200

    Fixed keymap for ZenMode

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>t', ':Neotree<CR>', {desc = 'Open Neo[T]ree'})
-vim.keymap.set('n', '<leader>Z', ':ZenMoDe<CR>', {desc = 'Toggle [Z]enMode'})
+vim.keymap.set('n', '<leader>Z', ':ZenMode<CR>', {desc = 'Toggle [Z]enMode'})
 

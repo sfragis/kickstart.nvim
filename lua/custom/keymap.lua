eaf3321a9f4164885e12dcb4214b58f6f9b508bc
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2024-10-27 13:20:15 +0100

    Neotree toggled

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -9,6 +9,6 @@
 --]]
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<leader>t', ':Neotree<CR>', {desc = 'Open Neo[T]ree'})
+vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', {desc = 'Open Neo[T]ree'})
 vim.keymap.set('n', '<leader>Z', ':ZenMode<CR>', {desc = 'Toggle [Z]enMode'})
 
